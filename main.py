0ffddc4facc555ee17112c77288e0ac7825661bc
pmiko <pio.mikolajczyk@gmail.com>
2025-04-02 17:50:36 +0200

    new fno model for ptb-xl dataset lead 0 signal - 60% acc for the first tryouts

--- main.py
+++ main.py
@@ -1,125 +1,145 @@
-# NOTE: Dataset from https://www.kaggle.com/datasets/shayanfazeli/heartbeat/code
-#  Publication: https://arxiv.org/pdf/1805.00794
+import ast
 import sys
+import time
+
 import matplotlib
+import wfdb
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn.metrics import confusion_matrix
-from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
+
 import torch
 from torch import nn, optim
 import torch.nn.functional as F
-from model import EcgClassifier
+import torch.cuda.amp as amp
+from model import EcgFNOClassifier
 
 matplotlib.use('TkAgg')
 
-def plot_correlation_matrix(df, graphWidth):
-    filename = df.dataframeName
-    df = df.dropna()
-    df = df[[col for col in df if df[col].nunique() > 1]]
-    if df.shape[1] < 2:
-        print(f'No correlation plots shown: The number of non-NaN or constant columns ({df.shape[1]}) is less than 2')
-        return
-    corr = df.corr()
-    plt.figure(num=None, figsize=(graphWidth, graphWidth), dpi=80, facecolor='w', edgecolor='k')
-    corrMat = plt.matshow(corr, fignum = 1)
-    plt.xticks(range(len(corr.columns)), corr.columns, rotation=90)
-    plt.yticks(range(len(corr.columns)), corr.columns)
-    plt.gca().xaxis.tick_bottom()
-    plt.colorbar(corrMat)
-    plt.title(f'Correlation Matrix for {filename}', fontsize=15)
-    plt.show()
 
-def plot_scatter_matrix(df, plotSize, textSize):
-    df = df.select_dtypes(include =[np.number])
-    df = df.dropna()
-    df = df[[col for col in df if df[col].nunique() > 1]]
-    columnNames = list(df)
-    if len(columnNames) > 10:
-        columnNames = columnNames[:10]
-    df = df[columnNames]
-    ax = pd.plotting.scatter_matrix(df, alpha=0.75, figsize=[plotSize, plotSize], diagonal='kde')
-    corrs = df.corr().values
-    for i, j in zip(*plt.np.triu_indices_from(ax, k = 1)):
-        ax[i, j].annotate('Corr. coef = %.3f' % corrs[i, j], (0.8, 0.2), xycoords='axes fraction', ha='center', va='center', size=textSize)
-    plt.suptitle('Scatter and Density Plot')
-    plt.show()
+def load_raw_data(df, sampling_rate, path):
+    if sampling_rate == 100:
+        data = [wfdb.rdsamp(path + f) for f in df.filename_lr]
+    else:
+        data = [wfdb.rdsamp(path + f) for f in df.filename_hr]
+    data = np.array([signal for signal, meta in data])
+    return data
 
-def plot_signal(df,row):
-    df = df.dropna()
-    plt.plot(df.iloc[row])
-    plt.show()
+def aggregate_diagnostic(y_dic):
+    tmp = []
+    for key in y_dic.keys():
+        if key in agg_df.index:
+            tmp.append(agg_df.loc[key].diagnostic_class)
+    return list(set(tmp))
 
+def one_hot_encode(labels, num_classes):
+    vector = np.zeros(num_classes, dtype=np.float32)
+    for label in labels:
+        vector[class_to_idx[label]] = 1.0
+    return vector
 
-# plot_correlation_matrix(df1, 10)
-# plot_scatter_matrix(df1, 10, 10)
-# plot_signal(df1)
+def count_parameters(model):
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-df_train = pd.read_csv('dataset/mitbih_train.csv', delimiter=',', nrows=None)
-df_test = pd.read_csv('dataset/mitbih_test.csv', delimiter=',', nrows=None)
+print("Using device:", device)
 
-df_train_data = df_train.iloc[:, :-1]
-df_train_labels = df_train.iloc[:,-1:]
+path = 'ptb-xl-dataset/'
+sampling_rate = 100
 
-df_test_data = df_test.iloc[:, :-1]
-df_test_labels = df_test.iloc[:,-1:]
+Y = pd.read_csv(path+'ptbxl_database.csv', index_col='ecg_id')
+Y.scp_codes = Y.scp_codes.apply(lambda x: ast.literal_eval(x))
 
-num_epochs = 20000
+X = load_raw_data(Y, sampling_rate, path)
+
+agg_df = pd.read_csv(path+'scp_statements.csv', index_col=0)
+agg_df = agg_df[agg_df.diagnostic == 1]
+
+
+Y['diagnostic_superclass'] = Y.scp_codes.apply(aggregate_diagnostic)
+
+test_fold = 10
+X_train = X[np.where(Y.strat_fold != test_fold)]
+X_test = X[np.where(Y.strat_fold == test_fold)]
+
+y_train = np.array(Y[(Y.strat_fold != test_fold)].diagnostic_superclass)
+y_test  = np.array(Y[Y.strat_fold == test_fold].diagnostic_superclass)
+
+unique_classes = np.unique(np.concatenate(Y['diagnostic_superclass'].values))
+print(unique_classes)
+num_classes = len(unique_classes)
+class_to_idx = {cls: idx for idx, cls in enumerate(unique_classes)}
+
+X_train_t = torch.tensor(X_train, dtype=torch.float32, device=device)
+X_test_t  = torch.tensor(X_test,  dtype=torch.float32, device=device)
+
+y_train_ohe = np.array([one_hot_encode(labels, num_classes) for labels in y_train])
+y_test_ohe  = np.array([one_hot_encode(labels, num_classes) for labels in y_test])
+
+y_train_t = torch.tensor(y_train_ohe, dtype=torch.float32, device=device)
+y_test_t  = torch.tensor(y_test_ohe,  dtype=torch.float32, device=device)
+
+##############################################################################
+# Note: Set up the model
+##############################################################################
+num_epochs = 10000
 batch_size = 64
-n_class = 5
-
-model = EcgClassifier(n_class)
+lead_view = 0
+modes = 7
+hidden_width = 100
+number_of_points = 1000
+model = EcgFNOClassifier(number_of_points,modes, hidden_width, num_classes).to(device)
+print('Model Parameters: ',count_parameters(model))
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=1e-3)
+optimizer = optim.Adam(model.parameters(), lr=5e-4)
 
 train_loss_history = []
-test_loss_history = []
-train_acc_history = []
-test_acc_history = []
+test_loss_history  = []
+train_acc_history  = []
+test_acc_history   = []
 
-for epoch in range(0,num_epochs):
-    df_test_data = df_test_data.reset_index(drop=True)
-    random_train_indices = np.random.choice(df_train.index, size=batch_size, replace=False)
-    random_test_indices = np.random.choice(df_test.index, size=batch_size, replace=False)
+#############################################################################
+# Note: Train loop
+##############################################################################
+start_block_time = time.time()
 
-    df_train_data_sample = df_train_data.loc[random_train_indices]
-    df_train_label_sample = df_train_labels.loc[random_train_indices]
+for epoch in range(num_epochs):
+    random_train_indices = torch.randperm(X_train_t.shape[0], device=device)[:batch_size]
+    random_test_indices  = torch.randperm(X_test_t.shape[0],  device=device)[:batch_size]
 
-    df_test_data_sample = df_test_data.loc[random_test_indices]
-    df_test_label_sample = df_test_labels.loc[random_test_indices]
+    train_data_sample = X_train_t[random_train_indices, :, lead_view]
+    train_label_sample = y_train_t[random_train_indices]
 
-    train_data_tensor = torch.from_numpy(df_train_data_sample.values).float().to(device)
-    train_label_tensor = torch.from_numpy(df_train_label_sample.values).long().to(device).squeeze(1)
+    test_data_sample = X_test_t[random_test_indices, :, lead_view]
+    test_label_sample = y_test_t[random_test_indices]
 
-    test_data_tensor = torch.from_numpy(df_test_data_sample.values).float().to(device)
-    test_label_tensor = torch.from_numpy(df_test_label_sample.values).long().to(device).squeeze(1)
-
-    one_hot_train_labels = F.one_hot(train_label_tensor, num_classes=n_class).float()
-    one_hot_test_labels = F.one_hot(test_label_tensor, num_classes=n_class).float()
-
-    # NOTE: MODEL TRAIN
     model.train()
-    train_outputs = model(train_data_tensor)
-    train_loss = criterion(train_outputs, one_hot_train_labels)
+    train_outputs = model(train_data_sample)
+    train_loss = criterion(train_outputs, train_label_sample)
+
     optimizer.zero_grad()
     train_loss.backward()
     optimizer.step()
-    _, train_preds = torch.max(train_outputs, dim=1)
-    train_correct = (train_preds == train_label_tensor).sum().item()
-    train_acc = train_correct / train_label_tensor.size(0)
 
-    # NOTE: MODEL VALIDATE
+    train_label_indices = torch.argmax(train_label_sample, dim=1)
+    _, train_preds = torch.max(train_outputs, dim=1)
+    train_correct = (train_preds == train_label_indices).sum().item()
+    train_acc = train_correct / train_label_indices.size(0)
+
+    # Evaluate
     model.eval()
     with torch.no_grad():
-        test_outputs = model(test_data_tensor)
-        test_loss = criterion(test_outputs, one_hot_test_labels)
-        _, test_preds = torch.max(test_outputs, dim=1)
-        test_correct = (test_preds == test_label_tensor).sum().item()
-        test_acc = test_correct / test_label_tensor.size(0)
+        test_outputs = model(test_data_sample)
+        test_loss = criterion(test_outputs, test_label_sample)
+
+    test_label_indices = torch.argmax(test_label_sample, dim=1)
+    _, test_preds = torch.max(test_outputs, dim=1)
+    test_correct = (test_preds == test_label_indices).sum().item()
+    test_acc = test_correct / test_label_indices.size(0)
 
     if epoch % 10 == 0:
         train_loss_history.append(train_loss.item())
@@ -127,53 +147,68 @@ for epoch in range(0,num_epochs):
         train_acc_history.append(train_acc)
         test_acc_history.append(test_acc)
 
-    if epoch % 50 == 0:
-        print(f'Epoch {epoch+1}/{num_epochs}, Train Loss: {train_loss.item():.4f}, Test Loss: {test_loss.item():.4f}')
+    if epoch % 50 == 0 and epoch > 0:
+        end_block_time = time.time()
+        block_elapsed = end_block_time - start_block_time
+        mean_block_time = block_elapsed / 50.0
+        print(
+            f"[{epoch:04d}/{num_epochs}] "
+            f"Train Loss: {train_loss.item():.4f} | "
+            f"Test Loss: {test_loss.item():.4f} | "
+            f"Train Acc: {train_acc:.2f} | Test Acc: {test_acc:.2f} | "
+            f"Mean Time/Epoch (last 50): {mean_block_time:.3f}s"
+        )
+        start_block_time = time.time()
 
-
-
+##############################################################################
+# Note: Plot losses / accuracies
+##############################################################################
 plt.style.use('dark_background')
 
 fig, axs = plt.subplots(1, 2, figsize=(8, 4))
 
-# Loss plot
-axs[0].plot( train_loss_history, label='Train Loss')
-axs[0].plot( test_loss_history, label='Test Loss')
-axs[0].set_xlabel('Epoch')
+axs[0].plot(train_loss_history, label='Train Loss')
+axs[0].plot(test_loss_history, label='Test Loss')
+axs[0].set_xlabel('Checkpoints (every 10 epochs)')
 axs[0].set_ylabel('Loss')
 axs[0].set_title('Training and Test Loss')
 axs[0].legend()
 axs[0].grid(True)
 
-axs[1].plot( train_acc_history, label='Train Accuracy')
-axs[1].plot( test_acc_history, label='Test Accuracy')
-axs[1].set_xlabel('Epoch')
+axs[1].plot(train_acc_history, label='Train Accuracy')
+axs[1].plot(test_acc_history, label='Test Accuracy')
+axs[1].set_xlabel('Checkpoints (every 10 epochs)')
 axs[1].set_ylabel('Accuracy')
 axs[1].set_title('Training and Test Accuracy')
 axs[1].legend()
 axs[1].grid(True)
 
-fig.suptitle("ECG CLASSIFIER RESULTS", fontsize=16)
-
+fig.suptitle("ECG FNO Classifier Results", fontsize=16)
 plt.tight_layout()
 plt.show()
 
-
+##############################################################################
+# Note: Confusion matrix on a subset of test data
+##############################################################################
 model.eval()
+no_samples_conf_matrix = 1000
 with torch.no_grad():
-    test_data_tensor_full = torch.from_numpy(df_test_data.values).float().to(device)
-    test_label_tensor_full = torch.from_numpy(df_test_labels.values).long().to(device)
-    outputs_full = model(test_data_tensor_full)
+    random_test_indices = torch.randperm(X_test_t.shape[0], device=device)[:no_samples_conf_matrix]
+    test_data_sample = X_test_t[random_test_indices, :, lead_view]
+    test_label_sample = y_test_t[random_test_indices]
+    outputs_full = model(test_data_sample)
     _, preds_full = torch.max(outputs_full, dim=1)
+    test_label_indices = torch.argmax(test_label_sample, dim=1)
 
-cm = confusion_matrix(test_label_tensor_full.cpu().numpy(), preds_full.cpu().numpy())
+cm = confusion_matrix(test_label_indices.cpu().numpy(), preds_full.cpu().numpy())
+
 plt.figure(figsize=(8, 6))
 plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
 plt.title('Confusion Matrix')
 plt.colorbar()
-tick_marks = np.arange(n_class)
-plt.xticks(tick_marks, [str(i) for i in range(n_class)])
-plt.yticks(tick_marks, [str(i) for i in range(n_class)])
+tick_marks = np.arange(num_classes)
+plt.xticks(tick_marks, [str(i) for i in range(num_classes)])
+plt.yticks(tick_marks, [str(i) for i in range(num_classes)])
 plt.ylabel('True label')
 plt.xlabel('Predicted label')
 
@@ -186,13 +221,3 @@ for i in range(cm.shape[0]):
 
 plt.tight_layout()
 plt.show()
-
-
-
-
-
-
-
-
-
-
--- model.py
+++ model.py
@@ -1,20 +1,69 @@
 import torch
-from mpmath import sigmoid
 from torch import nn
 
-
-class EcgClassifier(nn.Module):
-    def __init__(self, no_labels: int):
+class SpectralConv1d(nn.Module):
+    def __init__(self, in_channels, out_channels, modes):
         super().__init__()
-        self.layer_0 = nn.Linear(187,100)
-        self.layer_1 = nn.Linear(100, 50)
-        self.layer_2 = nn.Linear(50, 10)
-        self.layer_3 = nn.Linear(10, no_labels)
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.modes = modes
+        self.scale = 1.0 / (in_channels * out_channels)
+        self.weights = nn.Parameter(
+            self.scale * torch.rand(in_channels, out_channels, modes, dtype=torch.cfloat)
+        )
 
-    def forward(self,x:torch.Tensor):
-        x = self.layer_0(x)
-        x = torch.relu(self.layer_1(x))
-        x = torch.relu(self.layer_2(x))
-        x = self.layer_3(x)
+    def forward(self, x):
+        batch_size, in_channels, n = x.shape
+        x_ft = torch.fft.rfft(x, dim=-1)
+        out_ft = torch.zeros(
+            batch_size, self.out_channels, x_ft.shape[-1],
+            dtype=torch.cfloat, device=x.device
+        )
+        out_ft[..., :self.modes] = torch.einsum("bim, ioj -> bom", x_ft[..., :self.modes], self.weights)
+        x_out = torch.fft.irfft(out_ft, n, dim=-1)
+        return x_out
+
+class EcgFNOClassifier(nn.Module):
+    def __init__(self, number_of_points, modes, width, no_labels):
+        super().__init__()
+        self.modes = modes
+        self.width = width
+        self.no_labels = no_labels
+        self.no_points = number_of_points
+        self.fc0 = nn.Linear(1, width)
+        self.sconv1 = SpectralConv1d(width, width, modes)
+        self.w1 = nn.Conv1d(width, width, kernel_size=1)
+        self.conv1 = nn.Conv1d(width, width // 2, kernel_size=3, padding=1)
+        self.sconv2 = SpectralConv1d(width // 2, width // 2, modes)
+        self.w2 = nn.Conv1d(width // 2, width // 2, kernel_size=1)
+        self.pool = nn.AdaptiveAvgPool1d(4)
+        self.fc1 = nn.Linear((width // 2) * 4, width // 4)
+        self.fc2 = nn.Linear(width // 4, no_labels)
+
+    def forward(self, x):
+        x = x.unsqueeze(-1)
+        x = self.fc0(x)
+        x = self.act(x)
+        x = x.permute(0, 2, 1)
+        r = x
+        x1 = self.sconv1(x)
+        x2 = self.w1(x)
+        x = x1 + x2
+        x = self.act(x + r)
+        x = self.conv1(x)
+        x = self.act(x)
+        r = x
+        x1 = self.sconv2(x)
+        x2 = self.w2(x)
+        x = x1 + x2
+        x = self.act(x + r)
+        x = self.pool(x)
+        x = x.reshape(x.shape[0], -1)
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.fc2(x)
         return x
 
+    @staticmethod
+    def act(x):
+        return torch.relu(x)
